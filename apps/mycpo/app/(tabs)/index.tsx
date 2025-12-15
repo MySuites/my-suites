e3feb34b8ddec193441635c8b7d4f9d6f4727a62
Zhicheng Li <Zli40547@gmail.com>
2025-12-15 13:31:19 -0500

    chore: empty HomeScreen component by removing its UI and imports.

--- apps/mycpo/app/(tabs)/index.tsx
+++ apps/mycpo/app/(tabs)/index.tsx
@@ -1,12 +1,5 @@
-import { ThemedText } from '../../components/ui/ThemedText';
-import { ThemedView } from '../../components/ui/ThemedView';
-import { SharedButton } from '@mycsuite/ui';
-
 export default function HomeScreen() {
   return (
-    <ThemedView className="flex-1 items-center justify-center">
-      <ThemedText type="title">Tab One</ThemedText>
-      <SharedButton title="This is a v4 NativeWind button!" />
-    </ThemedView>
+    <></>
   );
 }
