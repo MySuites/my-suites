3ba7f28afd544f4aaefc14dd2cee84b11e0d6975
Zhicheng Li <Zli40547@gmail.com>
2026-03-03 16:34:32 -0500

    refactor: Stop automatically setting editing mode after adding exercises.

--- apps/myhealth/app/workouts/details.tsx
+++ apps/myhealth/app/workouts/details.tsx
@@ -131,7 +131,6 @@ export default function CreateWorkoutScreen() {
             exercises.forEach(exercise => {
                 addExercise(exercise);
             });
-            setIsEditing(true);
         }
         setIsAddingExercise(false);
     }
